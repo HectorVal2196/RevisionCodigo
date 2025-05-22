5f7378a68f584038f7d4dbfedb21379551c49d44
Hector Valdez <hectorvaldez1907@gmail.com>
2025-05-22 13:47:46 -0600

    se hacen los cambios para que funcione

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,37 @@
+// URL de la API de GitHub
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
+
+const $n = document.querySelector('#name'); // se agrega eñ signo de # para indicar que es un id  y pueda ser seleccionado
+const $b = document.querySelector('#blog'); // como contenia el # se asigno el id en el html
+const $l = document.querySelector('.location'); // se agrego la clase en el html 
+
+// Se agrego async a la función p<ara poder usar await
+async function displayUser(username) {
   $n.textContent = 'cargando...';
+
+
+try{
   const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
-}
+  const data = await response.json(); // Se agrego await para obtener los datos en formato Json
+  console.log(data); // mostrar los datos en la consola
+
+  // Se deben usar backticks en lugar de comillas simples 
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
+} catch (err) {
+  handleError(err);
+
+  } //catch
+} //async
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+
+  $n.textContent = `Algo salió mal: ${err}` // se agrego el $ en la variable para que pueda ser seleccionada
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski'); //se borra el catch que no es necesario porque el try/catch esta dentro
